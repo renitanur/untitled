52048cc92381df59b998ce3413c58a7be6b3ff04
HP COREi3 <r42615211@gmail.com>
2022-11-14 19:48:09 +0700

    answer3

--- Answer3.java
+++ Answer3.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Answer3 {
+public class Answer3 {
+
+        public class DataKendaraan {
+            public static void main(String[] args) {
+                String[] infoPlat = new String[]{"K-Kudus","B-Jakarta", "AB-Jogja", "T-Karawang", "AD-Solo"};
+            }
+
+            String PlatMobil1 = "K = Kudus";
+            String PlatMobil2 = "B = Jakarta";
+            String PlatMobil3= "AB = Jogja";
+            String PlatMobil4 = "T = Karawang";
+            String PlatMobil5 = "AD = Solo";
+
+            public static void PlatMobil(){
+                int platKudus =0;
+                for (int index=0;index<platKudus;index++){
+                }
+                int platJakarta=0;
+                for (int index=0;index<platJakarta;index++){
+
+                }
+                int platJogja =0;
+                for (int index=0;index<platJogja;index++){
+
+                }
+                int platKarawang =0;
+                for (int index=0;index<platKarawang;index++){
+
+                }
+                int platSolo =0;
+                for (int index=0;index<platSolo;index++){
+
+                }
+
+                System.out.println("Data PlatMobil :");
+            }
+
+        }
+
+    }
+
+
 }
